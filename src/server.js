ea1aeafff83ee2413fab329c511e9c5d253bafd9
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-02-10 02:12:08 +0700

    adsfasdf

--- src/server.js
+++ src/server.js
@@ -13,26 +13,22 @@ const app = express();
 // Session secret
 const sessionSecret = process.env.SESSION_SECRET || crypto.randomBytes(32).toString("hex");
 
-// Enable CORS
-// app.use(cors());
-// app.use(cors({
-//   origin: "*", // This allows all origins
-//   credentials: true, // Allows cookies to be sent
-//   methods: ["GET", "POST", "PUT"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// }));
-app.use(
-  cors({
-    origin: "*", // Allows all origins
-    // origin: "http://localhost:5000", // Allows all origins
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Allow cookies and authorization headers
-  })
-);
+var corsOptions = {
+	origin: function (origin, callback) {
+		let whitelist_array = String(process.env.WHITELIST).split(" ").join("").split(",");
+		if (whitelist_array.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('cors'))
+		}
+	}
+}
 
 // Middleware
 app.use(express.json());
+
+app.use(cors({ methods: "POST, GET, PUT" }));
+
 
 // Initialize session middleware with MongoDB store
 app.use(
@@ -61,8 +57,8 @@ app.use(passport.session());
 connectDB();
 
 // Import Routes
-const userRoutes = require("./routes/user.routes");
-const adminRoutes = require("./routes/admin.routes");
+const userRoutes = require("./routes/user.routes", cors(corsOptions));
+const adminRoutes = require("./routes/admin.routes", cors(corsOptions));
 
 // Use Routes
 app.get('/', (req, res) => {
