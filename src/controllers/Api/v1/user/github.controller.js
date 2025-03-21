59e1523afd2515881ebe3d51f871c2fb507f23ea
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2025-03-21 15:48:29 +0700

    feat: refactor GitHub callback function to streamline user authentication process

--- src/controllers/Api/v1/user/github.controller.js
+++ src/controllers/Api/v1/user/github.controller.js
@@ -5,52 +5,6 @@ const { generateToken } = require('../../../../utils/jwt.util');
 // GitHub login function
 const githubLogin = passport.authenticate('github', { scope: ['user:email'] });
 
-// GitHub callback function
-// const githubCallback = passport.authenticate('github', { failureRedirect: 'http://localhost:3001' });
-// const githubCallback = (req, res, next) => {
-//     passport.authenticate('github', async (err, user, info) => {
-//         if (err) {
-//             return next(err); // Handle any Passport-specific errors
-//         }
-
-//         if (!user) {
-//             return res.status(401).send('Authentication failed: User not found');
-//         }
-//         try {
-//             // Check if the user already exists in the database
-//             let existingUser = await User.findOne({ github_id: user.id, email: user.emails[0].value });
-//             console.log("=============== existingUser ================");
-//             console.log(existingUser);
-//             if (!existingUser) {
-//                 console.log("=============== Create User before create ================");
-//                 // Create a new user if not found
-//                 existingUser = new User({
-//                     github_id: user.id,
-//                     username: user.username,
-//                     email: user.emails[0].value, // Use the first email from GitHub
-//                 });
-//                 await existingUser.save();
-//                 console.log("=============== Create User ================");
-//                 console.log(existingUser);
-//             }
-
-//             // Log the user in
-//             // req.logIn(existingUser, (err) => {
-//             //     if (err) {
-//             //         return next(err); // Handle login errors
-//             //     }
-//             //     return res.redirect(redirectURL); // Redirect to success URL
-//             // });
-//             const token = generateToken(existingUser);
-//             console.log("=============== token ================");
-//             console.log(token);
-//             res.redirect(redirectURL + `#token=${token}`);
-//         } catch (error) {
-//             return next(error); // Pass the error to the error-handling middleware
-//         }
-//     })(req, res, next); // Invoke Passport's authenticate function
-// };
-
 const githubCallback = (req, res, next) => {
     passport.authenticate('github', async (err, user, info) => {
         if (err) {
